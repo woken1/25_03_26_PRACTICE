ae8ca544b5ca13c6124b9390afc2c9083103c299
woken1 <82443038+woken1@users.noreply.github.com>
2026-03-25 15:02:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,18 @@ bool test1()
   return true;
 }
 
+bool test2()
+{
+  Vector< int > v;
+  return v.isEmpty();
+}
+
 int main()
 {
   using test_t = bool(*)();
   test_t tests[] = {
-    test1
+    test1,
+    test2
   };
   constexpr size_t count = sizeof(tests) / sizeof(test_t)
   for (size_t i = 0; i < count; ++i){
