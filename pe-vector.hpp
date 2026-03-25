dc368f91d69312b2519875f874c4c0130ac6f865
woken1 <82443038+woken1@users.noreply.github.com>
2026-03-25 15:07:01 +0300

    Update pe-vector.hpp

--- pe-vector.hpp
+++ pe-vector.hpp
@@ -1,5 +1,7 @@
 #ifndef PE_VECTOR_HPP
 #define PE_VECTOR_HPP
+#include <cstdef>
+
 namespace knk {
   template< class T >
   class Vector {
@@ -11,6 +13,16 @@ namespace knk {
     T* data;
     size_t size, capacity;
   };
+}
+template< class T >
+bool knk::Vector< T >::isEmpty() const except {
+  return false;
+}
+knk::Vector< T >::Vector():
+  data(nullptr),
+  size_(0),
+  capacity_(0)
+  delete[] data_;
 }
 
 #endif
