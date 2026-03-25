6f97a3fb8559f69beef99d2111e74f36899b6c52
woken1 <82443038+woken1@users.noreply.github.com>
2026-03-25 14:51:46 +0300

    const/dest tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
-template< class T >
-struct Vector {
-  T* data;
-  size_t size, capacity;
-};
+#include "pe-vector.hpp"
+#include <iostream>
+
+bool test1()
+{
+  knk::Vector< int > v;
+  return true;
+}
 
 int main()
 {
-  
+  using test_t = bool(*)();
+  test_t tests[] = {
+    test1
+  };
+  constexpr size_t count = sizeof(tests) / sizeof(test_t)
+  for (size_t i = 0; i < count; ++i){
+    bool r = tests[i]()
+    if (!r) {
+      std::count << "Failed: " << i << "\n";
+    }
+  }
 }
 
